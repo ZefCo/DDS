80122afb312a4a7df4d6820c5092da04f608c335
ZefCo <zefram.cochrane2024@gmail.com>
2022-10-19 17:34:22 -0500

    done with warmup and taking current measurement

--- SFTests/SFTest_v1.cpp
+++ SFTests/SFTest_v1.cpp
@@ -24,71 +24,72 @@ int count_digits(int number) {
 int main() {
     int R = 20, C = 20, Z = 1;
 
-    // int max_r = count_digits(R);
-    // int max_c = count_digits(C);
-    // int max_z = count_digits(Z);
+    std::vector<std::vector<int>> fuckNRin;
+    std::vector<std::vector<int>> fuckNRout;
 
-    NRMat3d<DP> fuckNR(1, R, C);
-    // Vec_IO_DP fuckNR;
-
-    // std::vector<std::vector<int>> data;
-    // std::vector<std::vector<int>> copy_data;
-
-
-    // // std::cout << data[1][2][0] << std::endl;
-    // // std::cout << "Passed" << std::endl;
+    
+    fuckNRin.resize(R);
+    for (int r = 0; r < R; r++) {
+        fuckNRin[r].resize(C);
+    }
 
     std::cout << "Init Lattice" << std::endl;
     for (int r = 0; r < R; r++) {
         for (int c = 0; c < C; c++) {
-            // fuckNR[r][c] = 0.0;
-            // std::cout << r << "x" << c << std::endl;
             
-            if (c < 10) {fuckNR[0][r][c] = 1;}
-            else {fuckNR[0][r][c] = 0;}
+            if (c < 10) {fuckNRin[r][c] = (double)1;}
+            else {fuckNRin[r][c] = (double)0;}
 
-            // std::cout << r << "x" << c << " = " << fuckNR[r][c] << std::endl;
         }
     }
-    // data.resize(R);
-    // for (int r = 0; r < R; r++) {
-    //     data[r].resize(C);
-    // }
 
-    // for (int r = 0; r < R; r++) {
-    //     for (int c = 0; c < C; c++) {
-    //         if (c < 10) {data[r][c] = 1;}
-    //         else {data[r][c] = 0;}
-    //     }
-    // }
-
-    std::cout << "Printing Lattice" << std::endl;
+    std::cout << "\nPrinting Lattice\n" << std::endl;
     
     for (int r = 0; r < R; r++) {
         for (int c = 0; c < C; c++) {
-            if (fuckNR[0][r][c] == 1) {std::cout << "@ ";}
+            if (fuckNRin[r][c] == 1) {std::cout << "@ ";}
             else {std::cout << "  ";}
         }
         std::cout << std::endl;
     }
+
+    for (int r = 0; r < R; r++) {
+        std::vector<double> copyRow(fuckNRin[r].begin(), fuckNRin[r].end());
+
+        std::cout << "Row: " << r << std::endl;
+        for (auto item: copyRow) {std::cout << item << " ";}
+        std::cout << std::endl;
+    }
+
+    for (int c = 0; c < C; c++) {
+        std::vector<double> copyCol;
+        for (int r = 0; r < R; r++) {
+            copyCol.push_back(fuckNRin[r][c]);
+        }
+
+        std::cout << "T Col: " << c << std::endl;
+        for (auto item: copyCol) {std::cout << item << " ";}
+        std::cout << std::endl;
+
+    }
     
-    std::cout << std::endl;
+    // std::cout << std::endl;
 
-    Mat3D_IO_DP copy_data = fuckNR;
-    Mat_IO_DP speq(1, 2*R);
+    // std::cout << "Fuck Numerical Recipies" << std::endl;
 
-    // for (int s = 0; s < 2*R; s++) {speq[0][s] = 0;}
-    std::cout << "Fuck Numerical Recipies" << std::endl;
+    // // fftw_plan fftw_plan_dft_2d()
 
-    // try {
-    //     std::cout << "Working on FFT" << std::endl;
-    //     // rlft3(copy_data, speq, 1);}
-    // catch(exception& exc) {std::cout << "Yep, fuck Numerical Recipies" << std::endl;}
-    // // NR::rlft3(copy_data, speq, 1);
+    // // try {
+    // //     std::cout << "Working on FFT" << std::endl;
+    // //     NR::rlft3(fuckNRin, speq, 1);}
+    // // catch(exception& exc) {std::cout << "Yep, fuck Numerical Recipies" << std::endl;}
+    // // // NR::rlft3(copy_data, speq, 1);
     
 
-    std::cout << "Finished beng fucked by Numerical Recipies" << std::endl;
+    // // std::cout << "Finished beng fucked by Numerical Recipies" << std::endl;
 
-    // NR::fourn(fuckNR, );
+    // // // NR::fourn(fuckNRin, );
+
+    // std::cout <<"No but seriously fuck NR" << std::endl;
 
 }
